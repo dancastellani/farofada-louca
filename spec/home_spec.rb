b571d90e3a9bfb12d3956ded07c654b3a0295a8f
Daniel Castellani <dancastellani@gmail.com>
2013-10-07 12:09:29 -0300

    Added base path to home_tests

--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -2,16 +2,23 @@
 
 require 'helper'
 
+Capybara.app_host = 'http://bomnegocio.com'
+
 describe 'Home Page', :type => :feature do
 
   it "should have message \'Tem o que pra vender?\'" do
-    visit 'http://www.bomnegocio.com'
+    visit '/'
     page.should have_content('Tem o que pra vender?')
   end
 
   it "should have a link to RJ state" do
-    visit 'http://www.bomnegocio.com'
+    visit '/'
     page.should have_link('RJ')
+  end
+
+  it "should have message \'A cada 1 minuto 4 coisas vendem\'" do
+    visit '/'
+    page.should have_content('A cada 1 minuto 4 coisas vendem')
   end
 end
 
