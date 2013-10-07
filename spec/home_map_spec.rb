40d8972b1c8c5b4cdeac894f39ba742723715693
Daniel Castellani <dancastellani@gmail.com>
2013-10-07 17:57:28 -0300

    Changed the name of internal_page test

--- spec/home_map_spec.rb
+++ spec/home_map_spec.rb
@@ -3,7 +3,8 @@
 require 'helper'
 
 describe 'Home Page direct state links', :type => :feature do
-      
+    
+    #TODO: Put the rest of the links
     [   { :link => "RJ", :expected_content => "Rio de Janeiro"},
         { :link => "SP", :expected_content => "São Paulo"} ].each do |test|
         it "should follow link #{test[:link]} to page #{test[:expected_content]}" do
