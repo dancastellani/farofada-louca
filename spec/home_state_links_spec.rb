e2e4008959045457c095063cad33899a902b242b
Daniel Castellani <dancastellani@gmail.com>
2013-10-10 14:37:50 -0300

    Comented map test. Not working with SVG nav. yet.

--- spec/home_state_links_spec.rb
+++ spec/home_state_links_spec.rb
@@ -3,32 +3,38 @@
 require_relative 'spec_helper'
 
 describe 'Home Page - State Links', :type => :feature do
-    
-    #TODO: Put the rest of the links
-    [   { :link => "RJ", :expected_content => "Rio de Janeiro"},
-        { :link => "SP", :expected_content => "São Paulo"}
-    ].each do |test|
-        it "should follow link #{test[:link]} to page #{test[:expected_content]}" do
-            visit '/'
-            click_link test[:link]
-            page.should have_content test[:expected_content]
-        end
-    end
-    
-end
 
-
-describe 'Home Page::Maps Links', :type => :feature do
-    
-    #TODO: Put the rest of the links
-    [   { :link => "RJ", :expected_content => "Rio de Janeiro"},
-    { :link => "SP", :expected_content => "São Paulo"}
-    ].each do |test|
-        it "should follow link #{test[:link]} to page #{test[:expected_content]}" do
-            visit '/'
-            click_link test[:link]
-            page.should have_content test[:expected_content]
+    describe 'Direct links', :type => :feature do
+        
+        #TODO: Put the rest of the links
+        [   { :link => "RJ", :expected_content => "Rio de Janeiro"},
+            { :link => "SP", :expected_content => "São Paulo"}
+        ].each do |test|
+            it "should follow link #{test[:link]} to page #{test[:expected_content]}" do
+                visit '/'
+                click_link test[:link]
+                page.should have_content test[:expected_content]
+            end
         end
+        
     end
-    
+
+=begin
+    describe 'Map navigation', :type => :feature do
+        
+        #TODO: Put the rest of the links
+        [   { :map_link => "estado_rj", :expected_content => "Rio de Janeiro"},
+        { :link => "estado_sp", :expected_content => "São Paulo"}
+        ].each do |test|
+            it "should follow map link to page #{test[:expected_content]}" do
+                visit '/'
+                #page.save_screenshot('screenshot.png')
+                page.execute_script(script)
+                page.should have_content test[:expected_content]
+            end
+        end
+        
+    end
+=end
+
 end
