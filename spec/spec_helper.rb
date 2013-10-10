137a41cab08c7a2082d42fe6cd3375c3c25ffeda
Daniel Castellani <dancastellani@gmail.com>
2013-10-10 16:26:34 -0300

    Added config to run with different borwsers (firefox, chrome, poltergeist). Example: BROWSER=firefox bundle exec rake

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,11 +11,30 @@ end
 
 require 'rspec'
 require 'capybara/rspec'
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
-  #Capybara::Selenium::Driver.new(app, :browser => :chrome)
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
+Capybara.register_driver :selenium_firefox do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, [:js_errors => true])
+end
+
+case ENV['BROWSER']
+  when 'chrome'
+  	puts "Capybara: Using Selenium Chrome Driver"
+    Capybara.default_driver = Capybara.javascript_driver = :selenium_chrome
+  when 'firefox'
+  	puts "Capybara: Using Selenium Firefox Driver"
+    Capybara.default_driver = Capybara.javascript_driver = :selenium_firefox
+  else
+  	puts "Capybara: Using Poltergeist (headless) Driver"
+    Capybara.default_driver = Capybara.javascript_driver = :poltergeist
+end
+ 
 require 'capybara-screenshot'
 require 'capybara-screenshot/rspec'
 Capybara::Screenshot.register_filename_prefix_formatter(:rspec) do |example|
@@ -27,9 +46,6 @@ Capybara.save_and_open_page_path = "temp/test-error-screenshots/"
 
 require 'capybara/poltergeist'
 Capybara.run_server = false
-#Capybara.default_driver = :selenium
-Capybara.default_driver = :poltergeist
-Capybara.javascript_driver = :poltergeist
 
 # prod
 Capybara.app_host = 'http://www2.bomnegocio.com'
